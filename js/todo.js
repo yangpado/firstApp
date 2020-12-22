f52ccdc9e1fec53f8ca55456242d7c37f9656508
yangpado <70185607+yangpado@users.noreply.github.com>
2020-12-22 11:20:30 +0900

    submit & save todolist

--- js/todo.js
+++ js/todo.js
@@ -4,16 +4,31 @@ const todoForm = document.querySelector('.js-todoForm'),
 
 const TODO_LS = 'todos';
 
+const todos = [];
+
+function saveTodos(){
+    localStorage.setItem(TODO_LS,JSON.stringify(todos));
+
+}
+
 function paintTodo(text){
     //console.log(text);
     const li = document.createElement('li');
     const delBtn = document.createElement('button');
     delBtn.innerText = '❌';
     const span = document.createElement('span');
+    const newId = todos.length + 1;
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
+    li.id = newId;
     todoList.appendChild(li);
+    const todoObj = {
+        text : text,
+        id : newId
+    };
+    todos.push(todoObj);
+    saveTodos();
 }
 
 function handleTodoSubmit(event){
@@ -24,9 +39,13 @@ function handleTodoSubmit(event){
 }
 
 function loadTodos(){
-    const todos = localStorage.getItem(TODO_LS);
-    if (todos !== null){
-        
+    const loadedtodos = localStorage.getItem(TODO_LS);
+    if (loadedtodos !== null){
+        //console.log(loadTodos);
+        const parsedTodos = JSON.parse(loadedtodos);
+        parsedTodos.forEach(function(toDo){
+            paintTodo(toDo.text);
+        });
     }
 }
 function init(){
