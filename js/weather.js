7d7430bb95304f81993b570ad62c3f123d7b16bb
yangpado <70185607+yangpado@users.noreply.github.com>
2020-12-22 17:50:48 +0900

    get weather API

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,19 @@
+const weather = document.querySelector('.js-weather');
+
 const API_KEY = '2f2b6b67a02bb5807af50c7cb0c59bdf';
 const COORDS = 'coords';
+
+function getWeather(lat, lng){
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`)
+        .then(function(response){
+            return response.json();
+        }).then(function(json){
+            const temperature = json.main.temp;
+            const place = json.name;
+            weather.innerText = `${temperature} @ ${place}`;
+        });
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,6 +27,7 @@ function handleGeoSucces(position){
         longitude : longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 function handleGeoError(){
     console.log('Cant access geo location');
@@ -27,6 +42,9 @@ function loadCoords(){
         askForCoords();
     } else {
         //getWeather
+        const parseCoords = JSON.parse(loadedCords);
+        //console.log(parseCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
