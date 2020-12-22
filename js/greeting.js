a994602e0770b78f52595a674a42c6fa1fac30a1
yangpado <70185607+yangpado@users.noreply.github.com>
2020-12-22 10:05:48 +0900

    getusername from form & save localstorage

--- js/greeting.js
+++ js/greeting.js
@@ -5,6 +5,23 @@ const form = document.querySelector('.js-form'),
 const USER_LS = 'currentUser',
     SHOWING_CN = 'showing';
 
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
+    
+function handleSubmit(event){
+    event.preventDefault(); //기본동작 막기
+    const currentValue = input.value;
+    //console.log(currentValue);
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener('submit',handleSubmit)
+}
+
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
@@ -15,6 +32,7 @@ function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
     if (currentUser === null) {
         // She is not
+        askForName();
 
     } else {
         // She is
